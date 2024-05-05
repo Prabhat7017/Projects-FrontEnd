41433ddb280b9791661427069000b3b638948199
Prabhat7017 <prabhatkumarmjp@gmail.com>
2024-05-05 23:24:13 +0530

    New project DevDetective (Fetch github profile) added

---
+++ Github profile fetch/index.js
@@ -0,0 +1,36 @@
+const url = "https://api.github.com/users/";
+
+function darkModeProperties() {
+    root.setProperty("--lm-bg", "#141D2F");
+    root.setProperty("--lm-bg-content", "#1E2A47");
+    root.setProperty("--lm-text", "white");
+    root.setProperty("--lm-text-alt", "white");
+    root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+    modetext.innerText = "LIGHT";
+    modeicon.src = "./assets/images/sun-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(1000%)");
+    darkMode = true;
+    console.log("darkmode changed to " + darkMode);
+    localStorage.setItem("dark-mode", true);  console.log("setting dark mode to false");
+  
+    console.log("setting dark mode to true");
+  
+  }
+  
+  //SWITCH TO LIGHT MODE - activateLightMode()
+  function lightModeProperties() {
+    root.setProperty("--lm-bg", "#F6F8FF");
+    root.setProperty("--lm-bg-content", "#FEFEFE");
+    root.setProperty("--lm-text", "#4B6A9B");
+    root.setProperty("--lm-text-alt", "#2B3442");
+    root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+    modetext.innerText = "DARK";
+    modeicon.src = "./assets/images/moon-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(100%)");
+    darkMode = false;
+    console.log("darkmode changed to " + darkMode);
+  
+    localStorage.setItem("dark-mode", false);
+    console.log("setting dark mode to false");
+  }
+  
